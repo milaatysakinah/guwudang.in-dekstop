53e946a479c730486b9841fc2ccc3244f6fb10de
Hakim Akbaru Sulthony <hakimakbar21@gmail.com>
2020-11-30 09:37:39 +0700

    List Partner Fixes #6

---
+++ guwudang.in-desktop/Partner/PartnerController.cs
@@ -0,0 +1,42 @@
+﻿using Velacro.Api;
+using Velacro.Basic;
+using System.Net.Http;
+using guwudang.Model;
+using System.Collections.Generic;
+
+
+namespace guwudang.Partner
+{
+    public class PartnerController : MyController
+    {
+        public PartnerController(IMyView _myView) : base(_myView)
+        {
+            //getData();
+        }
+
+        public async void partner()
+        {
+            var client = new ApiClient("http://127.0.0.1:8000/");
+            var request = new ApiRequestBuilder();
+
+            var req = request
+                .buildHttpRequest()
+                .setEndpoint("api/partner/")
+                .setRequestMethod(HttpMethod.Get);
+            client.setOnSuccessRequest(setViewPartnerData);
+            var response = await client.sendRequest(request.getApiRequestBundle());
+            //Console.WriteLine(response.getJObject().ToString());
+            //client.setAuthorizationToken(response.getJObject().ToString());
+        }
+
+        private void setViewPartnerData(HttpResponseBundle _response)
+        {
+            if (_response.getHttpResponseMessage().Content != null)
+            {
+                string status = _response.getHttpResponseMessage().ReasonPhrase;
+                getView().callMethod("setPartner", _response.getParsedObject<List<guwudang.Model.Partner>>());
+            }
+        }
+
+    }
+}
---
+++ guwudang.in-desktop/Partner/PartnerPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using Velacro.UIElements.Basic;
+using Velacro.UIElements.Button;
+using Velacro.UIElements.TextBlock;
+using Velacro.UIElements.TextBox;
+
+
+namespace guwudang.Partner
+{
+    /// <summary>
+    /// Interaction logic for ProductPage.xaml
+    /// </summary>
+    public partial class PartnerPage : MyPage
+    {
+        private BuilderButton buttonBuilder;
+        private BuilderTextBox txtBoxBuilder;
+        private BuilderTextBlock txtBlockBuilder;
+        private IMyButton newPartnerButton;
+        private IMyButton deletePartnerButton;
+        private IMyTextBox searchPartnerTxtBox;
+        private IMyTextBox passwordTxtBox;
+        private IMyTextBlock loginStatusTxtBlock;
+
+        public PartnerPage()
+        {
+            InitializeComponent();
+            this.KeepAlive = true;
+            //List<Product> items = new List<Product>();
+            //items.Add(new Product() { id = "1", product_type_id = "1", user_id = "1", product_name = "Baju Badut", price = "120000", units = "24", description = "Ini Deskripsi product 1", product_picture = "/img/", created_at = "", updated_at = "" });
+            //lvProduct.ItemsSource = items;
+            setController(new PartnerController(this));
+            initUIBuilders();
+            initUIElements();
+            getPartner();
+        }
+
+        private void initUIBuilders()
+        {
+            buttonBuilder = new BuilderButton();
+            txtBoxBuilder = new BuilderTextBox();
+        }
+
+        private void initUIElements()
+        {
+            newPartnerButton = buttonBuilder.activate(this, "newPartnerBtn").addOnClick(this, "");
+            deletePartnerButton = buttonBuilder.activate(this, "deletePartnerBtn").addOnClick(this, "");
+        }
+
+        private void getPartner()
+        {
+            getController().callMethod("partner");
+        }
+
+        public void setPartner(List<Model.Partner> partners)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                lvPartner.ItemsSource = partners;
+            });
+        }
+
+
+    }
+
+    //public class Product
+    //{
+    //    public string id { get; set; }
+    //    public string product_type_id { get; set; }
+    //    public string user_id { get; set; }
+    //    public string product_name { get; set; }
+    //    public string price { get; set; }
+    //    public string units { get; set; }
+    //    public string description { get; set; }
+    //    public string product_picture { get; set; }
+    //    public string created_at { get; set; }
+    //    public string updated_at { get; set; }
+
+    //}
+}
