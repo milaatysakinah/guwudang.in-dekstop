5b16e507b63be240018e874876ba3111674db766
Izzat <Izzat@DESKTOP-1M4IGLQ>
2020-12-09 19:58:23 +0700

    Layout

---
+++ guwudang.in-desktop/Account/AccountPageController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Velacro.Api;
+using Velacro.Basic;
+using System.Net.Http;
+using guwudang.Model;
+using System.Collections.Generic;
+using guwudang.utils;
+using System;
+using System.Windows.Controls;
+
+namespace guwudang.Account
+{
+    public class AccountPageController : MyController
+    {
+        private static string id;
+        public AccountPageController(IMyView _myView) : base(_myView)
+        {
+            //getData();
+        }
+
+        public async void getAccountData()
+        {
+            var client = new ApiClient("http://localhost:8000/");
+            var request = new ApiRequestBuilder();
+
+            User user = new User();
+            string token = user.getToken();
+            client.setAuthorizationToken(token);
+
+            var reqAccount = request
+                .buildHttpRequest()
+                .setEndpoint("api/authUser")
+                .setRequestMethod(HttpMethod.Get);
+
+
+        }
+    }
+}
