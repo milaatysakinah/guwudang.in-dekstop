3e3c9357aad32275d3e1b35855c2d68df44b9518
Izzat <Izzat@DESKTOP-1M4IGLQ>
2020-12-17 20:20:42 +0700

    page

---
+++ guwudang.in-desktop/utils/EPages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Velacro.UIElements.Basic;
+
+namespace guwudang.utils
+{
+    public class EPages
+    {
+        public static string listDashboardPage = "DASHBOARD";
+        public static string listInvoicePage = "INVOICE";
+        public static string listPartnerPage = "PARTNER";
+        public static string listProductPage = "PRODUCT";
+        public static string listAccountPage = "ACCOUNT";
+
+    }
+}
---
+++ guwudang.in-desktop/utils/PageManagement.cs
@@ -0,0 +1,41 @@
+﻿using guwudang.Account;
+using guwudang.Invoice;
+using guwudang.Partner;
+using guwudang.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Velacro.DataStructures;
+using Velacro.UIElements.Basic;
+
+namespace guwudang.utils
+{
+    public class PageManagement
+    {
+        private static bool initialized;
+        private static MyDictionary<string, MyPage> pages;
+
+        private static void initPages()
+        {
+            pages = new MyDictionary<string, MyPage>();
+            pages.Add(utils.EPages.listDashboardPage, new Dashboard.Dashboard());
+            pages.Add(utils.EPages.listAccountPage, new AccountPage());
+            pages.Add(utils.EPages.listInvoicePage, new ListInvoicePage());
+            pages.Add(utils.EPages.listProductPage, new ProductPage());
+            pages.Add(utils.EPages.listPartnerPage, new PartnerPage());
+            initialized = true;
+        }
+
+        public static MyPage getPage(string _page)
+        {
+            if (!initialized)
+            {
+                initPages();
+            }
+            return pages[_page];
+        }
+
+    }
+}
